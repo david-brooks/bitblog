f5041eb86b48d4fcbcbb51a104dc59e793adea81
David Brooks <davidbrooks.now@gmail.com>
2014-01-29 12:58:13 -0500

    initial commit

---
+++ bitblog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bitblog::Application.config.secret_key_base = '0d9f8c7df1fb159155157132915f8744a5b7f9059b7c622809791afc11081a7af7355d6fe368aac5550a0fda138f26d1d5b17926fe18994a8b99c398fe3e0871'
---
+++ bitblog/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bitblog::Application.config.session_store :cookie_store, key: '_bitblog_session'
